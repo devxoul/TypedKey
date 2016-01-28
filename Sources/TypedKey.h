5fd8289297f9458a2f4dcdd2f8142adc57a40924
Suyeol Jeon <devxoul@gmail.com>
2016-01-28 16:34:32 +0900

    Start Xcode project

---
+++ Sources/TypedKey.h
@@ -0,0 +1,19 @@
+//
+//  TypedKey.h
+//  TypedKey
+//
+//  Created by 전수열 on 1/28/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TypedKey.
+FOUNDATION_EXPORT double TypedKeyVersionNumber;
+
+//! Project version string for TypedKey.
+FOUNDATION_EXPORT const unsigned char TypedKeyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TypedKey/PublicHeader.h>
+
+
